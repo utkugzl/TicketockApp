17f87ee5b26e95969270552f31a3a815af917466
Utku Guzel <utku.guzel44@gmail.com>
2023-09-26 18:00:33 +0300

    Initial Commit

---
+++ Ticketock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ticketock
+//
+//  Created by Utku Güzel on 26.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
